470987bd55af41caa6c0373b847f093784e0ab98
Yo Kamada <bumpfuji10@gmail.com>
2024-01-28 22:53:32 +0900

    検証console.log削除

--- src/api/article.ts
+++ src/api/article.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const url = "https://lbt0630zfb.microcms.io/api/v1/articles";
 
 export const getArticles = () => {
-  console.log(import.meta.env.VITE_HEADLESS_CMS_API_KEY)
   return axios.get(
     url,
     {
