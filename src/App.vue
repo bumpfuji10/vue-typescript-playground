2cd71435e29cfffa40f6584f944f16bae40e6f62
Yo Kamada <bumpfuji10@gmail.com>
2024-02-01 23:04:21 +0900

    Headerコンポーネントのパス移動

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import Header from './components/Header.vue'
+import Header from './views/Header.vue'
 </script>
 
 <template>
---
+++ src/views/Header.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+export default {
+  name: 'Header',
+}
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class="blog_title">Kyam Site</h1>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+  .blog_title {
+    text-align: center;
+  }
+
+  a {
+    text-decoration: none;
+    color: hsla(160, 100%, 37%, 1);
+    transition: 0.4s;
+    padding: 8px;
+  }
+
+
+  nav {
+    font-size: 16px;
+    text-align: center;
+    margin-top: 8px;
+  }
+</style>
