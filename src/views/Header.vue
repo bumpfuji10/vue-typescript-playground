2d7381ba65e4e993860f3c1bf044e5207d31d6e4
Yo Kamada <bumpfuji10@gmail.com>
2024-02-01 23:07:38 +0900

    一旦掃除

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -7,31 +7,7 @@ export default {
 <template>
   <header>
     <div class="wrapper">
-      <h1 class="blog_title">Kyam Site</h1>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <h1>Kyam Site</h1>
     </div>
   </header>
 </template>
-
-<style scoped>
-  .blog_title {
-    text-align: center;
-  }
-
-  a {
-    text-decoration: none;
-    color: hsla(160, 100%, 37%, 1);
-    transition: 0.4s;
-    padding: 8px;
-  }
-
-
-  nav {
-    font-size: 16px;
-    text-align: center;
-    margin-top: 8px;
-  }
-</style>
