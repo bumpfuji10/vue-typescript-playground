5159254348bcadec5f0fe68d7cb6eb754a8871a7
Yo Kamada <bumpfuji10@gmail.com>
2024-02-03 16:28:53 +0900

    headerデザインを簡単に変更

--- src/views/Header.vue
+++ src/views/Header.vue
@@ -7,7 +7,14 @@ export default {
 <template>
   <header>
     <div class="wrapper">
-      <h1 class="nav__title">Kyam Site</h1>
+      <h1 class="nav__title">
+        <router-link to="/" class="nav__link">
+          Kyam Site
+        </router-link>
+      </h1>
+      <router-link to="/about" class="nav__link">
+        About
+      </router-link>
     </div>
   </header>
 </template>
