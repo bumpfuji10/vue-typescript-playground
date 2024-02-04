e76222e53ba352d1daa120e0e0ee8b99634d7ef0
Yo Kamada <bumpfuji10@gmail.com>
2024-02-04 09:48:17 +0900

    articleで指定するURL変数名をアッパーケースに変更

--- src/api/article.ts
+++ src/api/article.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const url = "https://lbt0630zfb.microcms.io/api/v1/articles";
+const URL = "https://lbt0630zfb.microcms.io/api/v1/articles";
 
 export const getArticles = () => {
   return axios.get(
-    url,
+    URL,
     {
       headers: {"X-MICROCMS-API-KEY": import.meta.env.VITE_HEADLESS_CMS_API_KEY}
     }
