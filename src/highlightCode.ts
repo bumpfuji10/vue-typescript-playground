13ab217db412204c0423beaaa48c42509caa5395
Yo Kamada <bumpfuji10@gmail.com>
2024-02-07 23:11:56 +0900

    highlightCodeは記事がある場合のみに限定

--- src/highlightCode.ts
+++ src/highlightCode.ts
@@ -4,13 +4,16 @@ import hljs from 'highlight.js'
 import 'highlight.js/styles/hybrid.css';
 
 export const highlightCode = (HtmlContent: string) => {
-  const $ = load(HtmlContent)
-  $('pre code').each((_, elm) => {
-    const result = hljs.highlightAuto($(elm).text());
-    $(elm).html(result.value);
-    $(elm).addClass('hljs');
-  });
-  return {
-    content: $.html(),
-  };
+  if (HtmlContent) {
+    const $ = load(HtmlContent)
+    $('pre code').each((_, elm) => {
+      const result = hljs.highlightAuto($(elm).text());
+      $(elm).html(result.value);
+      $(elm).addClass('hljs');
+    });
+    return {
+      content: $.html(),
+    };
+  }
+  return false;
 }
